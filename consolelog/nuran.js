908a7eb04a3bcc38b4e53ff3703de943b50dd03e
Aykut Demirel <aykut.demirel@oredata.com>
2022-01-10 13:12:07 +0300

    First Commit

---
+++ consolelog/nuran.js
@@ -0,0 +1,5 @@
+console.log ("Nuran Demirel");
+console.log (15);
+var x=5;
+var y=7;
+console.log(x+y);
---
+++ degiskentanimlama/nuran.js
@@ -0,0 +1,17 @@
+let gun=08;
+let ay=08;
+let yil=1986;
+
+console.log(gun,ay,yil);
+
+console.log("Nuran Demirel "+gun+"/"+ay+"/"+yil+" tarihinde dogmustur");
+
+gun=29;
+console.log(gun);
+
+var arabafiyati=130000;
+console.log(arabafiyati);
+
+const arabafiyati=25000; 
+
+console.log(arabafiyati);
